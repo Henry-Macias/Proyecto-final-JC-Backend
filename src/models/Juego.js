28230c911d49993309d4244b0b3c4fe9754e8e26
unknown <henrymaciaslucas@gmail.com>
2025-11-14 12:31:14 -0500

    agrego funciones a juegos.js

--- src/models/Juego.js
+++ src/models/Juego.js
@@ -1,11 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const juegoSchema = new mongoose.Schema({
-  titulo: String,
-  portada: String,
-  horasJugadas: Number,
-  puntuacion: Number,
-  completado: Boolean,
+  titulo: { type: String, required: true },
+  imagenPortada: { type: String, default: "" },
+
+  genero: { type: String, default: "" },
+  plataforma: { type: String, default: "" },
+  añoLanzamiento: { type: Number, default: null },
+  desarrollador: { type: String, default: "" },
+  descripcion: { type: String, default: "" },
+
+  horasJugadas: { type: Number, default: 0 },
+  puntuacion: { type: Number, default: 0 },
+  completado: { type: Boolean, default: false },
 });
 
-export default mongoose.model('Juego', juegoSchema);
+export default mongoose.model("Juego", juegoSchema);
